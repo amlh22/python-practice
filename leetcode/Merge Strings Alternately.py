6ccb34562513e09404cf4a42d313fdec0d235699
amlh22 <101705746+amlh22@users.noreply.github.com>
2024-09-06 21:50:48 +0800

    Create Merge Strings Alternately.py

---
+++ leetcode/Merge Strings Alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged_string = ""
+        i, j = 0, 0
+
+        while i < len(word1) and j < len(word2):
+            merged_string += word1[i] + word2[j]
+            i += 1
+            j += 1
+
+        #This appends the remaining letters onto the end of the string
+        merged_string += word1[i:] + word2[j:]
+
+        return merged_string
